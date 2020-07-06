7497f6b628db618ae78262e3024dd2e5b5f5ccc5
Paolavg <sagrav.paola@gmail.com>
2020-07-05 23:28:27 -0500

    Final commit

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,42 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## Creates the object to calculate the matrix inverse 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y){
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## Write a short comment describing this function:
+## solves the matrix inverse if its the first time
+## or retrieve the cached inverse matrix if it already has been calculated, 
+## also measures the execution time to evaluate the performance improve
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  start_time <- Sys.time()
+  print(start_time)
+  m <- x$getinverse()
+  if(!is.null(m)){
+    message("getting cached matrix")
+    end_time <- Sys.time()
+    print(end_time)
+    print(end_time - start_time)
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  end_time <- Sys.time()
+  print(end_time)
+  print(end_time - start_time)
+  m
 }
